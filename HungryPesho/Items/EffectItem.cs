42e65cff753f513b90eb99e08044668a6a575ba8
unbelt <unbelt@outlook.com>
2014-10-16 23:59:30 +0300

    EffectItem

---
+++ HungryPesho/Items/EffectItem.cs
@@ -0,0 +1,13 @@
+﻿namespace HungryPesho.Items
+{
+    using System;
+    using HungryPesho.Interfaces;
+
+    public abstract class EffectItem : Item, IEffectable
+    {
+        public EffectItem(int id, string name, string description)
+            : base(id, name, description)
+        {
+        }
+    }
+}
