34350aaac89ab66f074238742f2ef044e7dba5df
Siujade <siujade@gmail.com>
2014-10-17 01:02:24 +0300

    Updating Creatures

--- HungryPesho/Interfaces/ICreature.cs
+++ HungryPesho/Interfaces/ICreature.cs
@@ -1,9 +1,15 @@
-﻿namespace HungryPesho.Creatures
+﻿using System;
+
+namespace HungryPesho.Creatures
 {
     interface ICreature
     {
         string Name { get; set; }
 
-        string Description { get; set; }
+        double Health { get; set; }
+
+        double Mana { get; set; }
+
+        double Initiative { get; set; }
     }
 }
