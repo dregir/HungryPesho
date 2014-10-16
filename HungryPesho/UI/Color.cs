593e1d776e86f1a24f209ab29cee9add6057b9ba
Siujade <siujade@gmail.com>
2014-10-17 00:55:09 +0300

    Colooors for console

---
+++ HungryPesho/UI/Color.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace HungryPesho.UI
+{
+    public class Color
+    {
+        public static string ColorMe(string text, ConsoleColor color)
+        {
+            Console.ForegroundColor = color;
+            Console.Write(text + " ");
+            Console.ResetColor();
+            return "";
+        }
+    }
+}
