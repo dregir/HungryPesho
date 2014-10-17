913b46eef4106ebc3ee5cf4a46542bc89bf8e8f2
Nina Markova <ninamarkoff@users.noreply.github.com>
2014-10-17 21:16:08 +0300

    Added Armor.ToString

--- HungryPesho/Items/Armor.cs
+++ HungryPesho/Items/Armor.cs
@@ -1,5 +1,7 @@
 ﻿namespace HungryPesho.Items
 {
+    using System.Text;
+
     public class Armor : StatItem
     {
         private int armorProtection;
@@ -31,6 +33,18 @@
             {
                 armorType = value;
             }
+        }
+
+        public override string ToString()
+        {
+            var print = new StringBuilder();
+
+            print.AppendFormat("{0} - {1} \r\nStats \r\nDamage: {2}",
+                this.ArmorType, this.Description, this.ArmorProtection);
+
+            print.Append(base.ToString());
+
+            return print.ToString();
         }
     }
 }
